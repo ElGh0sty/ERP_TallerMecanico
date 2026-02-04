2da4a57051b1a7c8cf394608a8467ed72c163d39
Yerai <11yeraidavid@gmail.com>
2026-02-04 11:22:27 -0500

    Creacion de la clase Conexion.cs para la base de datos (creo que puede fallar por la falta de nombre en el servidor pero hay q probar)

---
+++ Conexion.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace PROYECTOMECANICO
+{
+    public class Conexion
+    {
+        // El punto (.) indica que use el servidor local de cada integrante
+        protected SqlConnection conectar = new SqlConnection("Server=.;Database=TallerMecanico_ERP;Integrated Security=True");
+
+        public void Abrir()
+        {
+            try { conectar.Open(); }
+            catch (Exception ex) { Console.WriteLine("Error al abrir: " + ex.Message); }
+        }
+
+        public void Cerrar()
+        {
+            try { conectar.Close(); }
+            catch (Exception ex) { Console.WriteLine("Error al cerrar: " + ex.Message); }
+        }
+    }
+}
