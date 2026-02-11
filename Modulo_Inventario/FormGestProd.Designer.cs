20b7043fcb5b54bfa6672ea9e532ea5622e6e099
Yerai <11yeraidavid@gmail.com>
2026-02-10 23:29:20 -0500

    Creacion del formulario Gestion Productos (nada mas)

---
+++ Modulo_Inventario/FormGestProd.Designer.cs
@@ -0,0 +1,48 @@
+﻿namespace PROYECTOMECANICO.Modulo_Inventario
+{
+    partial class FormGestProd
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // FormGestProd
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.ClientSize = new System.Drawing.Size(908, 603);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FormGestProd";
+            this.Text = "FormGestProd";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
